04bc85e5b1777f06bb5bbd444edd263d037dcbc8
liuisaac <80558145+liuisaac@users.noreply.github.com>
2024-09-05 22:06:05 -0700

    updated image source list

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["lh3.googleusercontent.com", "localhost", "storage.googleapis.com"],
+        domains: ["lh3.googleusercontent.com", "localhost", "storage.googleapis.com", 'mecha-mayhem-frontend.vercel.app'],
     }
 };
 
