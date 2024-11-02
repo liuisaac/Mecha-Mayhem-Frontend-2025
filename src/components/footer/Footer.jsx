e226970259ccc7bcdfabd19b0ba7a6bbeac3f4f8
cng04 <calebng.ca@gmail.com>
2024-11-02 17:13:45 -0400

    Made Icons larger and removed link to repo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from 'next/link'
 import React from "react";
 
 const Footer = () => {
@@ -15,11 +16,11 @@ const Footer = () => {
                 </figure>
                 » Mecha Mayhem 2024
             </div>
-            <div className="w-[90vw] h-2/3 border-b-2 border-white border-opacity-40 flex flex-col sm:flex-row items-center justify-start">
+            <div className="w-[90vw] h-2/3 border-b-2 border-white border-opacity-40 flex flex-col sm:flex-row items-center sm:items-start justify-start">
                 <div className="flex flex-col sm:items-start font-extralight text-base lg:text-xl sm:gap-6 sm:mr-12 mb-10 w-3/4 sm:w-1/4">
                     <span className="font-semibold flex items-start sm:w-full">Contact Us:</span>
                     <span className="flex flex-row items-center sm:flex-row-centered">
-                        <figure className="w-[1.5vw] h-[1.5vw] relative overflow-hidden mr-2">
+                        <figure className="w-[4vw] h-[4vw] sm:w-[1.5vw] sm:h-[1.5vw] relative overflow-hidden mr-2">
                             <Image
                                 src={"/footer/Gmail Icon.svg"}
                                 alt="A picture of our facility's stations"
@@ -30,7 +31,7 @@ const Footer = () => {
                         <span className="max-md:break-all">Mecha@westernmech.ca</span>
                     </span>
                     <span className="flex flex-row items-center sm:flex-row-centered">
-                        <figure className="w-[1.5vw] h-[1.5vw] relative overflow-hidden mr-2">
+                        <figure className="w-[4vw] h-[4vw] sm:w-[1.5vw] sm:h-[1.5vw] relative overflow-hidden mr-2">
                             <Image
                                 src={"/footer/Phone Icon.svg"}
                                 alt="A picture of our facility's stations"
@@ -44,7 +45,7 @@ const Footer = () => {
                 <div className="flex flex-col sm:items-start font-extralight text-base lg:text-xl sm:gap-6 sm:mr-12 mb-10 w-3/4 sm:w-1/4">
                     <span className="font-semibold flex items-start sm:w-full">Follow Us:</span>
                     <figure className="flex flex-row items-center sm:flex-row-centered">
-                        <figure className="w-[1.5vw] h-[1.5vw] relative overflow-hidden mr-2">
+                        <figure className="w-[4vw] h-[4vw] sm:w-[1.5vw] sm:h-[1.5vw] relative overflow-hidden mr-2">
                             <Image
                                 src={"/footer/Instagram Icon.svg"}
                                 alt="A picture of our facility's stations"
@@ -55,7 +56,7 @@ const Footer = () => {
                         <span className="max-md:break-all">@western_mechatronic</span>
                     </figure>
                     <figure className="flex flex-row items-center sm:flex-row-centered">
-                        <figure className="w-[1.5vw] h-[1.5vw] relative overflow-hidden mr-2">
+                        <figure className="w-[4vw] h-[4vw] sm:w-[1.5vw] sm:h-[1.5vw] relative overflow-hidden mr-2">
                             <Image
                                 src={"/footer/Youtube Icon.svg"}
                                 alt="A picture of our facility's stations"
@@ -68,8 +69,8 @@ const Footer = () => {
                 </div>
                 <div className="flex flex-col sm:items-start font-extralight text-base lg:text-xl sm:gap-6 sm:mr-12 mb-10 w-3/4 sm:w-1/4">
                     <span className="font-semibold flex items-start sm:w-full">Report an Issue:</span>
-                    <span className="flex flex-row items-center sm:flex-row-centered">
-                        <figure className="w-[1.5vw] sm:w-[1.5vw] h-[1.5vw] relative overflow-hidden mr-2">
+                    {/* <span className="flex flex-row items-center sm:flex-row-centered">
+                        <figure className="w-[4vw] h-[4vw] sm:w-[1.5vw] sm:h-[1.5vw] relative overflow-hidden mr-2">
                             <Image
                                 src={"/footer/Git Icon.svg"}
                                 alt="A picture of our facility's stations"
@@ -77,10 +78,10 @@ const Footer = () => {
                                 fill
                             />
                         </figure>
-                        <span className="max-md:break-all">liuisaac/Mecha_Mayhem_App/issues</span>
-                    </span>
+                        <Link href="https://github.com/liuisaac/Mecha_Mayhem_App_2024/issues">Report a bug</Link>
+                    </span> */}
                     <span className="flex flex-row items-center sm:flex-row-centered">
-                        <figure className="w-[1.5vw] h-[1.5vw] relative overflow-hidden mr-2">
+                        <figure className="w-[4vw] h-[4vw] sm:w-[1.5vw] sm:h-[1.5vw] relative overflow-hidden mr-2">
                             <Image
                                 src={"/footer/Star Icon.svg"}
                                 alt="A picture of our facility's stations"
